e9515e7887a62c9cd097d8eed88db3a83584e905
Amar Viswanathan <amarviswanathan@gmail.com>
2025-11-12 08:38:48 -0500

    Yaml hyperparameters

--- flappy/agent.py
+++ flappy/agent.py
@@ -3,11 +3,22 @@ from dqn import DQN
 from experience_replay import ReplayMemory
 import gymnasium
 import torch 
-import itertools 
+import itertools
+import yaml
 # env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
 device = torch.device('mps') if torch.backends.mps.is_available() else torch.device('cpu')
 
 class Agent:
+    def __init__(self,hyperparameters_set):
+        with open('hyperparameters.yaml', 'r') as file:
+            all_hyperparameters = yaml.safe_load(file)
+            hyperparameters = all_hyperparameters[hyperparameters_set]
+        self.replay_memory_size = hyperparameters['replay_memory_size']
+        self.mini_batch_size = hyperparameters['mini_batch_size']
+        self.epsilon_init = hyperparameters['epsilon_init']
+        self.epsilon_decay = hyperparameters['epsilon_decay']
+        self.epsilon_min = hyperparameters['epsilon_min']
+
     def run(self, is_training=True, render=False):
         env = gymnasium.make("CartPole-v1", render_mode="human" if render else None)
         num_states = env.observation_space.shape[0]
@@ -16,7 +27,7 @@ class Agent:
         policy_dqn = DQN(num_states, num_actions).to(device)
 
         if is_training:
-            memory = ReplayMemory(10000,seed=42)
+            memory = ReplayMemory(self.replay_memory_size,seed=42)
         
         for episode in itertools.count(1000):
             state, _ = env.reset()
